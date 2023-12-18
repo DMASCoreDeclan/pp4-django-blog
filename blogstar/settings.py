e710aed58fa554763bc122eedfd591f9fc5da1cd
DMASCoreDeclan Sweeney <declan.sweeney@gmail.com>
2023-12-18 10:37:36 +0000

    Add SECRET_KEY to env.py

--- blogstar/settings.py
+++ blogstar/settings.py
@@ -25,7 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*#vxvg@y#59w896re54km29l8mn1$rks4wu!8ny5&=4(sg+0lo'
+# Original SECRET_KEY
+# SECRET_KEY = 'django-insecure-*#vxvg@y#59w896re54km29l8mn1$rks4wu!8ny5&=4(sg+0lo'
+# New SECRET_KEY
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False # True

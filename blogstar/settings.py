0bc984a263f7e88415a9df47b2717df4db6d1c12
DMASCoreDeclan Sweeney <declan.sweeney@gmail.com>
2023-12-19 16:12:46 +0000

    Install Summernote to Django

--- blogstar/settings.py
+++ blogstar/settings.py
@@ -53,7 +53,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_summernote',
     'blog',
+    
 ]
 
 MIDDLEWARE = [

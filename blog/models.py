9c8db9c1bacac2718d79336d91dec34df324f056
DMASCoreDeclan Sweeney <declan.sweeney@gmail.com>
2023-12-19 13:03:53 +0000

    Add Order and dunder methods to the Comment model

--- blog/models.py
+++ blog/models.py
@@ -17,8 +17,13 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+
     class Meta:
         ordering = ["-created_on"]
+
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
 
 
 class Comment(models.Model):
@@ -31,5 +36,12 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(choices=APPROVED, default=0)
     created_on = models.DateTimeField(auto_now_add=True)
-    
+
+
+    class Meta:
+        ordering = ["-created_on"]
+
+
+    def __str__(self):
+        return f"Comment: {self.body} by {self.author}"
     

d330ffd0b654186c5caa0a97b252f4b0cc4c4063
DMASCoreDeclan Sweeney <declan.sweeney@gmail.com>
2023-12-19 19:36:13 +0000

    Add base.html, index.html, TEMPLATE_DIRS, STATIC and style.css

--- blog/admin.py
+++ blog/admin.py
@@ -6,10 +6,21 @@ from django_summernote.admin import SummernoteModelAdmin
 class PostAdmin(SummernoteModelAdmin):
 
     list_display = ('title', 'slug', 'status', 'created_on')
-    search_fields = ['title']
-    list_filter = ('status',)
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on')
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
+
+#    @admin.register(Comment)
+#    class PostAdmin(SummernoteModelAdmin):
+#
+#        list_display = ('post', 'author', 'created_on')
+#        search_fields = ['title', 'content']
+#        list_filter = ('approved', 'created_on')
+#       # prepopulated_fields = {'slug': ('title',)}
+#        summernote_fields = ('body',)
+
+
 
 # Register your models here.
 admin.site.register(Comment)
